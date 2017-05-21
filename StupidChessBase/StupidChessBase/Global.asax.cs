cfa7172721f5c1267fc88e2b3bbede8d0996c6ac
vladi99 <vlpetrovic99@gmail.com>
2017-05-21 13:31:43 +0300

    add automatic migration of db

--- StupidChessBase/StupidChessBase/Global.asax.cs
+++ StupidChessBase/StupidChessBase/Global.asax.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using StupidChessBase.Data;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using StupidChessBase.Data.Migrations;
 
 namespace StupidChessBase
 {
@@ -12,6 +15,7 @@ namespace StupidChessBase
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, DbMigrationsConfig>());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
