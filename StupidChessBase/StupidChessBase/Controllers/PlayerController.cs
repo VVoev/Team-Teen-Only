c6047770245584c872fca984b0b92c507c5dbde7
VVoev <vladovoev@yahoo.co.uk>
2017-05-24 13:43:25 +0300

    Added Players View

---
+++ StupidChessBase/StupidChessBase/Controllers/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using StupidChessBase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StupidChessBase.Controllers
+{
+    public class PlayerController : BaseController
+    {
+        public ActionResult Players()
+        {
+            var players = this.db.Players.OrderBy(x => x.Rating).Select(x => new PlayerViewModel()
+            {
+                FullName = x.FirstName + " " + x.LastName,
+                Rating = x.Rating,
+                Wins = x.Wins,
+                Loses = x.Loses,
+                Draws = x.Draws
+            });
+
+            return View(new PlayersViewModel()
+            {
+                PlayersData = players
+            });
+        }
+    }
+}
---
+++ StupidChessBase/StupidChessBase/Models/PlayersViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StupidChessBase.Models
+{
+    public class PlayersViewModel
+    {
+        public IEnumerable<PlayerViewModel> PlayersData { get; set; }
+    }
+}
