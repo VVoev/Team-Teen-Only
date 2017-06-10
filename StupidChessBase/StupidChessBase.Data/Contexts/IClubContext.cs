b1d3ff0bcf05d686f6c17f22db13a620da1383fc
vladi99 <vlpetrovic99@gmail.com>
2017-06-10 21:12:33 +0300

    Fix conflicts

---
+++ StupidChessBase/StupidChessBase.Data/Contexts/IClubContext.cs
@@ -0,0 +1,12 @@
+﻿using StupidChessBase.Data.Models.SqlLiteModels;
+using System.Data.Entity;
+
+namespace StupidChessBase.Data.Contexts
+{
+    public interface IClubContext
+    {
+        IDbSet<Club> Clubs { get; set; }
+
+        int SaveChanges();
+    }
+}
---
+++ StupidChessBase/StupidChessBase.Tests/Controllers/PlayerControllerTests/Constructor_Should.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using StupidChessBase.Controllers;
+using StupidChessBase.Data;
+using StupidChessBase.Data.Contexts;
+
+namespace StupidChessBase.Tests.Controllers.PlayerControllerTests
+{
+    [TestFixture]
+
+    public class Constructor_Should
+    {
+
+        [Test]
+        public void Constructor_ShouldCreateController_WhenNoValuesArePassed()
+        {
+            // Arrange
+            var controller = new PlayerController();
+
+            // Act & Assert
+            Assert.IsInstanceOf<PlayerController>(controller);
+            Assert.IsInstanceOf<IApplicationDbContext>(controller.db);
+            Assert.IsInstanceOf<IClubContext>(controller.liteDb);
+        }
+
+        [Test]
+        public void Constructor_ShouldCreateController_WhenValuesArePassed()
+        {
+            // Arrange
+            var db = new ApplicationDbContext();
+            var liteDb = new ClubContext();
+            var controller = new PlayerController(db, liteDb);
+
+            // Act & Assert
+            Assert.IsInstanceOf<PlayerController>(controller);
+            Assert.AreSame(controller.db, db);
+            Assert.AreSame(controller.liteDb, liteDb);
+        }
+    }
+}
